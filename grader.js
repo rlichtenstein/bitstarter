9e9fe107d6678d4383750db8e8dda400344bf6b5
Rich Lichtenstein <rlichtenstein@gmail.com>
2013-07-11 01:50:38 +0000

    Part 3 done

--- grader.js
+++ grader.js
@@ -27,7 +27,7 @@ var cheerio = require('cheerio');
 var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "index.html"; 
+var URL_DEFAULT = "blank";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -39,26 +39,28 @@ var assertFileExists = function(infile) {
 };
 
 
-var assertURLExists = function(inURL) {
-   var instr = inURL.toString();
- /*   if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }*/
-    return instr;
-};
-
-
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
-
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var cheerioHtmlFile = function(htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+
+var checkURL = function(result, checksfile) {
+    $ = cheerio.load(result);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -78,16 +80,25 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-	.option('-u, --url <url>', 'URL to check', clone(assertURLExists), URL_DEFAULT) 
+	.option('-u, --url <url>', 'URL to check', URL_DEFAULT) 
         .parse(process.argv);
     
     if (program.url != URL_DEFAULT) {
-	console.log("URL detected!");
+	rest.get(program.url).on('complete', function(result) {
+        if (result instanceof Error) {
+            console.log("Error loading %s",program.url);
+            process.exit(1);
+            } else {
+		var checkJson = checkURL(result, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	    }});
+    } else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
     }
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
 
-    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
